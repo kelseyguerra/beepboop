86aa9cdafec00839eff2a5f1333309c9d47658e3
kelsey guerra <kelseylguerra@gmail.com>
2018-04-05 18:29:11 -0700

    change js functions to work as instructed

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,35 @@
-
 // business-logic //
-var binary = ["0", "1"];
-function robotboy(number) {
-  var userArray = number.split("");
-  var output0 = "Beep!";
-  var output1 = "Boop!";
-  var output2 = "I'm sorry, ";
-  var output3= ". I'm afraid I can't do that.";
-  var nameInput = $("input#name").val();
-  var robotOutput = "";
-  var range = [];
-    for (var i = 0; i < userArray.length; i++) {
-      if (number % 3 === 0 && number > 0) {
-        robotOutput = output2 + nameInput + output3
 
-      } else if (userArray[i] === binary[1]) {
-        robotOutput = output1
-        break
-      } else if (userArray[i] === binary[0]) {
-        robotOutput = output0
-      } else {
-        for (var j = 0; j <= number; j++) {
-          range.push(j);
-          robotOutput = range
-        }
-      }
+function robotBoy(number) {
+  var beeps = "Beep!";
+  var boops = "Boop!";
+  var dave = "I'm sorry, dave. I'm afraid I can't do that.";
+  var robotArray = [];
+  for (var i = 0; i <= number; i++) {
+    var userString = i.toString().split("");
+    if (i % 3 === 0 && i > 0) {
+      robotArray.push(dave);
+    } else if (userString.includes("1")) {
+      robotArray.push(boops);
+    } else if (userString.includes("0")) {
+      robotArray.push(beeps)
+    } else {
+      robotArray.push(i)
     }
-  return robotOutput
+  }
+return robotArray;
 }
 
 
 
 // user-interface logic //
-$(document).ready(function() {
-  $("form#boop").submit(function(event) {
-    event.preventDefault();
-});
+
 $(function() {
   $("#boop").submit(function(event) {
     event.preventDefault();
-    var userInput = $("#input").val();
-    var robotSpeech = robotboy(userInput);
-    $("#output").append("<p>" + robotSpeech + "</p>");
-    $(".name").text(nameInput);
+    var userInput = parseInt($("#input").val());
+    var robotOutput = robotBoy(userInput);
+    $("#output").append("<p>[" + robotOutput + "]<p>");
+
+    });
   });
-});
